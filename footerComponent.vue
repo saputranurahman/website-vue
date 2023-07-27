4d2b3ac76dd7603c205d44eda7a90c4a506f9678
unknown <saputranurahman24@gmail.com>
2023-07-27 11:57:46 +0700

    Update Lagi

--- footerComponent.vue
+++ footerComponent.vue
@@ -1,6 +1,31 @@
-<script setup>
-import { Footer } from 'flowbite-vue'
-</script>
 <template>
-  <Footer></Footer>
+  
+<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+                <img src="https://www.pngmart.com/files/4/Nike-Logo-PNG-Pic.png" class="h-8 mr-3" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NIKE</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 "></a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Shop</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
 </template>
